382013950162fa858754fb6e53b9cf04482885da
RainyKnight <stoltmanlucas@gmail.com>
2021-11-02 22:20:30 -0700

    Update todo list

--- driver.cpp
+++ driver.cpp
@@ -1,20 +1,28 @@
 // Initialized by Lucas Stoltman October 31st 2021
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
+   
+   const string name = "Lucas";
 
-   std::cout << "Hello World" << std::endl;
+   cout << "Hello World" << endl;
    return 0;
 }
-
 
 // TODO List
 /*
 
-
-
-
+- const
+- operator overload
+- reference
+- pointers
+- vectors
+- protected
+- inheritance
 
 
 
