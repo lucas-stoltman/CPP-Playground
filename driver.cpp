6211d909f81492ea088f2e9d9c38f599a5b4d360
RainyKnight <stoltmanlucas@gmail.com>
2021-11-07 22:33:51 -0800

    Add value and pointer const examples

--- driver.cpp
+++ driver.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+
 #include "basic.h"
 
 using namespace std;
@@ -16,23 +17,35 @@ string& returnString(string& s) { return s; }
 // this method won't allow the return type to change
 const string& returnStringConst(string& s) { return s; }
 
+void print(string var, int number) { cout << var << ": " << number << endl; }
+
 int main() {
-   string s = "test";
-
-   cout << s << endl;
-
-   returnString(s) = "test2";
-   // returnStringConst(s) = "testConst";
-
-   cout << s << endl;
-
+   // -----------------------------------------------------------
+   // -----------------------Const Practice----------------------
    // -----------------------------------------------------------
 
+   // VALUES ----------------------------------------------------
+   const int A = 1;
+   // const values can't be changed
+   // A = 5; illegal
+   print("A", A);
+
+   // POINTERS --------------------------------------------------
+   int B = 2;
+   int* const ptrB = &B;
+   // const pointers can't point anywhere else
+   // ptrB = &A; illegal
+   print("B", B);
+
+   // but the value they point toward can still be changed
+   *ptrB = 3;
+   print("B", B);
+
+   // FUNCTIONS -------------------------------------------------
    Basic test(5);
 
-   test.printNum();
-   test.setNum(10);
-   test.printNum();
+   // ------------------------------------
+   int outside = 101;
 
    return 0;
 }
